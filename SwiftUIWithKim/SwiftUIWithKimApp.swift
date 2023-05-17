3f2b3d87f3bb7d6fa99fc545d2a59575b5514b2a
Kimleng <kimlenghor2006@gmail.com>
2023-05-17 12:21:44 -0500

    Initial Commit

---
+++ SwiftUIWithKim/SwiftUIWithKimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithKimApp.swift
+//  SwiftUIWithKim
+//
+//  Created by Kimleng Hor on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithKimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
